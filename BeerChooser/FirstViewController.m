8972404615cce4d7646ea986972f3b8f0ab7b4b4
Laura Skelton <lauraeskelton@gmail.com>
2014-07-07 17:21:44 -0400

    Initial Commit

---
+++ BeerChooser/FirstViewController.m
@@ -0,0 +1,29 @@
+//
+//  FirstViewController.m
+//  BeerChooser
+//
+//  Created by Laura Skelton on 7/7/14.
+//  Copyright (c) 2014 Laura Skelton. All rights reserved.
+//
+
+#import "FirstViewController.h"
+
+@interface FirstViewController ()
+
+@end
+
+@implementation FirstViewController
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ BeerChooser/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  BeerChooser
+//
+//  Created by Laura Skelton on 7/7/14.
+//  Copyright (c) 2014 Laura Skelton. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
